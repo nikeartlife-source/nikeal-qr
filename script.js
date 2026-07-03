0a3f9e117580aa3aa3169d4ed5e19030870e8e5c
nikeartlife-source <nikeartlife@gmail.com>
2026-07-03 10:23:37 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,23 @@
 document.getElementById("startButton").addEventListener("click", startCamera);
 
-function startCamera(){
-  alert("ボタンが押されました！");
+async function startCamera(){
+  try {
+    const stream = await navigator.mediaDevices.getUserMedia({
+      video: {
+        facingMode: "environment"
+      }
+    });
+
+    const video = document.createElement("video");
+    video.setAttribute("autoplay", true);
+    video.setAttribute("playsinline", true);
+    video.srcObject = stream;
+
+    const reader = document.getElementById("reader");
+    reader.innerHTML = "";
+    reader.appendChild(video);
+
+  } catch (err) {
+    alert("カメラ起動失敗\n\n" + err.name + "\n" + err.message );
+  }
 }
