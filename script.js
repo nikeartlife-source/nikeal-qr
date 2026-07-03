958eb15c36668f77d59dc9ab4f71ed120367a20e
nikeartlife-source <nikeartlife@gmail.com>
2026-07-03 10:43:24 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,29 @@
 document.getElementById("startButton").addEventListener("click", startCamera);
 
-async function startCamera(){
-  try {
-    const stream = await navigator.mediaDevices.getUserMedia({
-      video: {
-        facingMode: "environment"
-      }
-    });
+let scanner;
 
-    const video = document.createElement("video");
-    video.setAttribute("autoplay", true);
-    video.setAttribute("playsinline", true);
-    video.srcObject = stream;
+function startCamera(){
 
     const reader = document.getElementById("reader");
     reader.innerHTML = "";
-    reader.appendChild(video);
 
-  } catch (err) {
-    alert("カメラ起動失敗\n\n" + err.name + "\n" + err.message );
-  }
+    scanner = new Html5Qrcode("reader");
+  
+    scanner.start(
+      { facingMode: "environment" },
+      { fps: 10,
+        qrbox: 250
+      },
+      onScanSuccess,
+      function(error){
+        //読み取り中は何もしない
+      }
+    ).catch (function(err) {
+    alert("カメラ起動失敗\n\n" + err );
+  });
+}
+
+function onScanSuccess(decodedText){
+  scanner.stop();
+  document.getElementById("result").innerHTML = "読み取り成功!<br><br>" + decodedText;
 }
