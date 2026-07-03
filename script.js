5745e3042f3765788837f8e685790bc05b7d8d07
nikeartlife-source <nikeartlife@gmail.com>
2026-07-03 18:26:51 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+const GAS_URL = "https://script.google.com/macros/s/AKfycby_VpnfCPFYXOVXNM-34rlEqUwPAQ89iAh_y9a5ku2f3N7UT-xQwWhsHm6lv62p0j2m/exec";
 document.getElementById("startButton").addEventListener("click", startCamera);
 
 let scanner;
@@ -25,5 +26,40 @@ function startCamera() {
 
 function onScanSuccess(decodedText){
   scanner.stop();
-  document.getElementById("result").innerHTML = "読み取り成功!<br><br>" + decodedText;
+  document.getElementById("result").innerHTML = "会員を検索しています...";
+
+  fetch(GAS_URL, {
+      method: "POST",
+      headers: {
+          "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+          action: "findMemberByQr",
+          qrId: decodedText
+      })
+  })
+  .then(response => response.json())
+  .then(member => {
+
+      if(!member) {
+          document.getElementById("result").innerHTML = "❌ 会員が見つかりません";
+          return;
+      }
+
+      showMemberMenu(member);
+  })
+  .catch(error => {
+      document.getElementById("result").innerHTML = error;
+  });
 }
+
+function showMemberMenu(member) {
+    document.getElementById("result").innerHTML = `
+        <h2>ようこそ!</h2>
+        <p>${member.name} さん</p>
+
+        <button id="lessonButton">レッスン登録</button>
+        <button id="pointButton">ポイント購入</button>
+    `;
+}
+
