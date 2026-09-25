1a063fe60e365a212d70765919175656865a966e
nikeartlife-source <nikeartlife@gmail.com>
2026-09-25 19:25:54 +0900

    Update member-detail.js

--- member-detail.js
+++ member-detail.js
@@ -1,18 +1,31 @@
 const GAS_URL = "https://script.google.com/macros/s/AKfycby_VpnfCPFYXOVXNM-34rlEqUwPAQ89iAh_y9a5ku2f3N7UT-xQwWhsHm6lv62p0j2m/exec";
 
 const params = new URLSearchParams(location.search);
+
 const memberId = params.get("memberId");
 
 if(!memberId){
-    document.getElementById("result").innerHTML = "<p>会員情報が指定されていません。</p>";
+
+    document.getElementById("result").innerHTML =
+
+        "<p>会員情報が指定されていません。</p>";
+
 }else{
+
     fetch(GAS_URL, {
+
         method: "POST",
+
         mode: "cors",
+
         body: JSON.stringify({
+
             action: "getMemberById",
+
             memberId: memberId
+
         })
+
     })
 
     .then(response => response.json())
@@ -20,37 +33,64 @@ if(!memberId){
     .then(member => {
 
         if(!member){
-            document.getElementById("result").innerHTML = "<p>会員が見つかりませんでした。</p>";
-            
+
+            document.getElementById("result").innerHTML =
+
+                "<p>会員が見つかりませんでした。</p>";
+
             return;
+
         }
 
         document.getElementById("result").innerHTML = `
+
             <h2>${member.name} さん</h2>
+
             <p>フリガナ：${member.kana}</p>
+
             <p>会員種別：${member.memberType}</p>
+
             <p>入会日：${member.joinDate}</p>
+
             <p>現在の残りポイント：${member.remainingPoints}pt</p>
+
             <hr>
+
             <h3>会員QRコード</h3>
+
             <div id="qrcode"></div>
+
         `;
 
         new QRCode(
+
             document.getElementById("qrcode"),
 
             {
+
                 text: member.qrId,
+
                 width: 250,
+
                 height: 250
+
             }
+
         );
+
     })
 
     .catch(error => {
+
         console.error(error);
 
         document.getElementById("result").innerHTML =
+
             "通信エラー：" + error.message;
+
     });
+
 }
+
+
+
