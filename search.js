4081a1cb41ed1586022b1c66ca3e22388bfbc79b
nikeartlife-source <nikeartlife@gmail.com>
2026-09-25 18:33:44 +0900

    Update search.js

--- search.js
+++ search.js
@@ -1,3 +1,5 @@
+const GAS_URL = "https://script.google.com/macros/s/AKfycby_VpnfCPFYXOVXNM-34rlEqUwPAQ89iAh_y9a5ku2f3N7UT-xQwWhsHm6lv62p0j2m/exec";
+
 document.getElementById("searchButton").addEventListener("click", function(){
 
     const keyword = document.getElementById("keyword").value.trim();
@@ -7,7 +9,47 @@ document.getElementById("searchButton").addEventListener("click", function(){
         return;
     }
 
-    document.getElementById("result").innerHTML =
-        "検索しています...";
+    document.getElementById("result").innerHTML = "検索しています...";
+    fetch(GAS_URL, {
+        method: "POST",
+        mode: "cors",
+        body: JSON.stringify({
+            action: "searchMembers",
+            keyword: keyword
+        })
+    })
+
+    .then(response => response.json())
+    .then(members => {
+
+        if(members.length === 0){
+            document.getElementById("result").innerHTML = "<p>該当する会員が見つかりませんでした。</p>";
+            return;
+        }
+
+        let html = `
+            <h2>検索結果</h2>
+        `;
+
+        members.forEach(function(member){
+            html += `
+                <div>
+                    <hr>
+                    <h3>${member.name} さん</h3>
+                    <p>フリガナ：${member.kana}</p>
+                    <p>会員種別：${member.memberType}</p>
+                    <p>入会日：${member.joinDate}</p>
+                    <p>現在の残りポイント：${member.remainingPoints}pt</p>
+                </div>
+            `;
+        });
+
+        document.getElementById("result").innerHTML = html;
+    })
+
+    .catch(error => {
+        console.error(error);
+        document.getElementById("result").innerHTML = "通信エラー：" + error.message;
+    });
 });
 
