ae48fd88e148b5a0211aa7928d83434170cf26e0
nikeartlife-source <nikeartlife@gmail.com>
2026-09-25 15:38:41 +0900

    Create member.js

---
+++ member.js
@@ -0,0 +1,7 @@
+const GAS_URL = "https://script.google.com/macros/s/AKfycby_VpnfCPFYXOVXNM-34rlEqUwPAQ89iAh_y9a5ku2f3N7UT-xQwWhsHm6lv62p0j2m/exec";
+
+document.getElementById("registerButton").addEventListener("click", function(){
+
+    alert("会員登録処理はこれから作ります");
+
+});
